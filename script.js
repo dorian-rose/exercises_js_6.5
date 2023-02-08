a664dca96bbc669174f151e6410bed380d9d0bc0
dorian-rose <dorianrkelly@gmail.com>
2023-02-08 09:02:33 +0100

    exercises complete except final. exercise 1 to review

--- script.js
+++ script.js
@@ -42,49 +42,18 @@ function buildString() {
 }
 
 //Una función que permita mostrar un triángulo como el siguiente:
-function numberOfStarsIncrement() {
-  let n = 5; //parseInt(prompt("inserte un numero entero"));
+function numberOfAsterisksIncrement() {
+  let n = parseInt(prompt("inserte un numero entero"));
 
-  let stars = "0";
+  let asterisks = "";
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      console.log(i, j);
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      asterisks += "*";
     }
+    console.log(asterisks);
+    asterisks = "";
   }
 }
-
-/*function myFunction() {
-  var text = "";
-  var i;
-  var j;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 2; j++) {
-      text += "The number is i = " + i + " and j = " + j + "<br>";
-    }
-  }
-  console.log(text);
-}
-myFunction();*/
 
 //Una función que devuelva la diferencia en días entre dos fechas del mismo año (sólo tenemos en cuenta dia y mes)
-let first = prompt("Enter a date with format 'January 20'");
-let second = prompt("Enter a date with format 'January 20'");
-
-//In JavaScript, date objects are created with new Date().
-//Date  converts to number of milliseconds since beginning of January 1 1970
-
-let dateOne = new Date(first);
-console.log(dateOne);
-let dateTwo = new Date(second);
-console.log(dateTwo);
-
-// segundos = milisegundos/1000
-// minutos = segundos/60
-// horas = minutos/60
-// Días = horas/24
-
-const differenceInDays = Math.floor(
-  (dateOne - dateTwo) / (1000 * 60 * 60 * 24)
-);
-console.log(differenceInDays);
